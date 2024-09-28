56faddce5861058916b0f2347be732c57767f9f9
Dung Huynh Duc <dunghd.it@gmail.com>
2024-09-28 21:40:36 +0800

    refactor: Improve fetchContent function and error handling in web app

---
+++ Dockerfile
@@ -0,0 +1,35 @@
+FROM node:20.17.0-alpine as builder
+
+# Set up working directory
+RUN mkdir /app
+WORKDIR /app
+
+# Upgrade and install git
+RUN apk --no-cache add git
+
+# Copy essential files
+COPY package.json pnpm-lock.yaml pnpm-workspace.yaml watt.json ./
+COPY apps apps
+
+# Install dependencies and build
+RUN npm install -g pnpm
+RUN pnpm install
+ENV NODE_ENV=production
+RUN pnpm run build
+
+# Set up the production image
+FROM node:20.17.0-alpine
+WORKDIR /app
+
+# Copy build output from builder
+#
+COPY --from=builder /app .
+
+# Expose port for health check with fly.io
+EXPOSE 3042/tcp
+
+# Set SERVER_HOSTNAME environment variable
+ENV PORT=3042
+
+# Start the application
+CMD ["npm", "run", "start"]
--- apps/web/app/page.tsx
+++ apps/web/app/page.tsx
@@ -1,13 +1,25 @@
 import styles from "./page.module.css";
 
+// This URL is available only in wattpm
+const API_URL = "http://http-node.plt.local";
+
+/**
+ * Fetches content from the internal node API service.
+ * @returns {Promise<string>} The content retrieved from the API.
+ * @throws {Error} If the fetch operation fails or the response is invalid.
+ */
+async function fetchContent() {
+	const response = await fetch(API_URL, { cache: "no-store" });
+	const data = await response.json();
+	return data.content;
+}
+
 export default async function Home() {
-	// This is calling node api service (internal service)
-	const { content } = await (
-		await fetch("http://http-node.plt.local", { cache: "no-store" })
-	).json();
+	const content = await fetchContent();
+
 	return (
 		<div className={styles.page}>
-			<main className={styles.main}>${content}</main>
+			<main className={styles.main}>{content}</main>
 		</div>
 	);
 }
